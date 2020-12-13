280a3a331ebf24837ae4cbb92747b54b4a2e1fe9
TV Quiz PhD <tvquizphd@gmail.com>
2020-12-13 18:42:29 -0500

    fix functional component extra braces

--- components/tree_map_chart.js
+++ components/tree_map_chart.js
@@ -2,36 +2,35 @@ import sizeMe from 'react-sizeme';
 import Chart from "react-google-charts";
 import { useState, useEffect } from 'react';
 
-const TreeMapChart = (props) => { {
-    const { size, data, options} = props;
-    const { onKeyPress, onChartReady, onChartSelect} = props;
+const TreeMapChart = (props) => {
+  const { size, data, options} = props;
+  const { onKeyPress, onChartReady, onChartSelect} = props;
 
-    const [fakeControls, setFakeControls] = useState([]);
+  const [fakeControls, setFakeControls] = useState([]);
 
-    useEffect(() => {
-      // trigger chart rerender
-      setFakeControls([]);
-    }, [size.width, size.height]);
+  useEffect(() => {
+    // trigger chart rerender
+    setFakeControls([]);
+  }, [size.width, size.height]);
 
-    return (
-      <div style={{ position: 'relative', height: '100%' }}>
-        <div style={{ position: 'absolute', width: '100%', height: size.height }}>
-          <Chart
-            chartType="TreeMap"
-            controls={fakeControls}
-            width={`${size.width + 10}px`}
-            height={`${size.height}px`}
-            data={data}
-            options={options}
-            chartEvents={[
-              { eventName: "select", callback: onChartSelect },
-              { eventName: "ready", callback: onChartReady }
-            ]}
-          />
-        </div>
+  return (
+    <div style={{ position: 'relative', height: '100%' }}>
+      <div style={{ position: 'absolute', width: '100%', height: size.height }}>
+        <Chart
+          chartType="TreeMap"
+          controls={fakeControls}
+          width={`${size.width + 10}px`}
+          height={`${size.height}px`}
+          data={data}
+          options={options}
+          chartEvents={[
+            { eventName: "select", callback: onChartSelect },
+            { eventName: "ready", callback: onChartReady }
+          ]}
+        />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default sizeMe({ monitorHeight: true })(TreeMapChart);
